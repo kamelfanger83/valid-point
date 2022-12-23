7db217ce12e2cdbbf35629d8b2f5fc2002eba3f4
BuildTools <unconfigured@null.spigotmc.org>
2022-12-23 19:39:39 +0100

    Testwindow for gian

---
+++ untils/windowbuilder.py
@@ -0,0 +1,68 @@
+import os
+import pygame
+
+class WindowBuilder:
+    def __init__(self, screen):
+        self.screen = screen
+        self.background = None
+        self.text_list = []
+        self.image_list = []
+        self.button_list = []
+
+    def setBackground(self, background):
+        self.background = pygame.image.load(".\\data\\img\\"+background+".jpg").convert()
+        self.background = pygame.transform.scale(self.background, self.screen.get_size())
+
+    def addText(self, text, location, size, color):
+        font = pygame.font.SysFont("Arial", size)
+        text = font.render(text, True, color)
+
+        self.text_list.append([text, location])
+
+    def addImage(self, path, location):
+        image = pygame.image.load(".\\data\\img\\" + path + ".jpg").convert()
+        self.image_list.append([image, location])
+
+    def addButton(self, id, text, textsize, textcolor, location, height, width, backgroundcolor):
+        rect = pygame.Rect(location[0], location[1], width, height)
+
+        font = pygame.font.SysFont("Arial", textsize)
+        text = font.render(text, True, textcolor)
+
+        self.button_list.append([id, rect, backgroundcolor, text, textsize, location, height, width])
+    def draw(self):
+        if self.background != None:
+            self.screen.blit(self.background, (0, 0))
+
+        for i in range(len(self.text_list)):
+            self.screen.blit(self.text_list[i][0], self.text_list[i][1])
+
+        for i in range(len(self.image_list)):
+            self.screen.blit(self.image_list[i][0], self.image_list[i][1])
+
+        for i in range(len(self.button_list)):
+            pygame.draw.rect(self.screen, self.button_list[i][2], self.button_list[i][1])
+            location = [(self.button_list[i][5][0] + (self.button_list[i][7]-self.button_list[i][4])/2), (self.button_list[i][5][1] + (self.button_list[i][6]-self.button_list[i][4])/2)]
+            self.screen.blit(self.button_list[i][3], location)
+
+        pygame.display.update()
+        pygame.time.Clock().tick(60)
+
+    def getEvents(self):
+        events = []
+        for i in range(len(self.button_list)):
+            startx = self.button_list[i][5][0]
+            starty = self.button_list[i][5][1]
+            endx = startx + self.button_list[i][7]
+            endy = starty + self.button_list[i][6]
+
+            x, y = pygame.mouse.get_pos()
+
+            if pygame.mouse.get_pressed()[0] and x > startx and x < endx and y > starty and y < endy:
+                events.append(["button_right_click", self.button_list[i][0]])
+            elif pygame.mouse.get_pressed()[2] and x > startx and x < endx and y > starty and y < endy:
+                events.append(["button_left_click", self.button_list[i][0]])
+            elif pygame.mouse.get_pressed()[1] and x > startx and x < endx and y > starty and y < endy:
+                events.append(["button_middle_click", self.button_list[i][0]])
+
+        return events
--- windows/testwindow.py
+++ windows/testwindow.py
@@ -1,16 +1,27 @@
 import buttons
 import pygame
-import sprites
+import untils.windowbuilder
+
+window = None
+def init(bigSprite, screen, title_size, activewindow):
+    global window
+    window = untils.windowbuilder.WindowBuilder(screen)
+    window.setBackground("background")
+
+    window.addText("Hello World!", (0, 0), 50, (255, 255, 255))
+    window.addText("How are you?", (0, 50), 25, (0, 255, 0))
+
+    window.addButton("button1", "Menu", 25, (0, 0, 0), (100, 200), 25, 25, (255, 255, 255))
+
+    return "show_test"
 
 def show(bigSprite, screen, tile_size, activewindow):
-    keys = pygame.key.get_pressed()
+    global window
+    window.draw()
 
-    """if keys[pygame.K_SPACE]:
-        activewindow = "init_game"""
-
-    bigSprite["menu"].draw(screen, (0, 0))
-
-    pygame.display.update()
-    pygame.time.Clock().tick(60)
-
+    for event in window.getEvents():
+        if event[0] == "button_right_click":
+            print("Right pressed: " + event[1])
+        elif event[0] == "button_left_click":
+            print("Left pressed: " + event[1])
     return activewindow
